27e7c1b6f225f906e0b5eb33467370ee3e9a3c7f
SARATHI R <sarathirofficial@gmail.com>
2024-07-18 10:32:39 +0530

    Connected database

---
+++ Backend/database/connectDatabase.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectDatabase = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`🛢️ Database connected: ${connect.connection.host}`);
+  } catch (err) {
+    console.error(`Error: ${err.message}`);
+    process.exit(1);
+  }
+};
