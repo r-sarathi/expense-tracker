dd7cf709c890e7544d5d93126f172ac3c6d1a73f
SARATHI R <sarathirofficial@gmail.com>
2024-07-20 12:58:31 +0530

    Added routes to the page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,24 @@
-import { ModeToggle } from "./components/mode-toggle";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Transaction from "./pages/Transaction";
+import NotFound from "./pages/NotFound";
+import Header from "./components/Header";
 
 const App = () => {
+  const authUser = true;
   return (
-    <div>
-      <ModeToggle />
-    </div>
+    <>
+      {authUser && <Header />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/transaction/:id" element={<Transaction />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 };
 
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ThemeProvider } from "./components/theme-provider.jsx";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
