87560ceb5329292dc00630d567b56db5cf862aa2
SARATHI R <sarathirofficial@gmail.com>
2024-07-24 11:44:30 +0530

    Created Signup functionalities

--- Frontend/src/pages/Signup.jsx
+++ Frontend/src/pages/Signup.jsx
@@ -9,8 +9,29 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { useState } from "react";
 
 const Signup = () => {
+  const [data, setData] = useState({
+    name: "",
+    username: "",
+    password: "",
+    gender: "",
+  });
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setData((prevData) => ({
+      ...prevData,
+      [id]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(data);
+  };
+
   return (
     <div className="h-screen flex items-center">
       <Card className="mx-auto max-w-sm md:w-3/4">
@@ -24,7 +45,14 @@ const Signup = () => {
           <div className="grid gap-4">
             <div className="grid gap-2">
               <Label htmlFor="email">Full Name</Label>
-              <Input id="name" type="name" placeholder="Full Name" required />
+              <Input
+                id="name"
+                type="text"
+                placeholder="Full Name"
+                required
+                value={data.name}
+                onChange={handleChange}
+              />
             </div>
             <div className="grid gap-2">
               <Label htmlFor="email">Username</Label>
@@ -33,30 +61,47 @@ const Signup = () => {
                 type="username"
                 placeholder="Username"
                 required
+                value={data.username}
+                onChange={handleChange}
               />
             </div>
             <div className="grid gap-2">
               <Label htmlFor="password">Password</Label>
-              <Input id="password" type="password" />
+              <Input
+                id="password"
+                type="password"
+                required
+                value={data.password}
+                onChange={handleChange}
+              />
             </div>
             <div className="grid gap-2">
               <Label htmlFor="password">Gender</Label>
-              <RadioGroup className="flex flex-row gap-4 ml-1">
+              <RadioGroup
+                className="flex flex-row gap-4 ml-1"
+                value={data.gender}
+                onValueChange={(value) =>
+                  setData({
+                    ...data,
+                    gender: value,
+                  })
+                }
+              >
                 <div className="flex items-center space-x-2">
-                  <RadioGroupItem value="option-one" id="option-one" />
+                  <RadioGroupItem value="Male" id="Male" />
                   <p className="text-sm" htmlFor="option-one">
                     Male
                   </p>
                 </div>
                 <div className="flex items-center space-x-2">
-                  <RadioGroupItem value="option-two" id="option-two" />
+                  <RadioGroupItem value="Female" id="Female" />
                   <p className="text-sm" htmlFor="option-two">
                     Female
                   </p>
                 </div>
               </RadioGroup>
             </div>
-            <Button type="submit" className="w-full">
+            <Button type="submit" className="w-full" onClick={handleSubmit}>
               Create an account
             </Button>
             <Button variant="outline" className="w-full">
