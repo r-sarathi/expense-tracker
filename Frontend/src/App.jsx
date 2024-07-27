1fc0f7be7394837c70f99f2112d639f24f817c14
SARATHI R <sarathirofficial@gmail.com>
2024-07-27 08:54:42 +0530

    Created user mutation

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,21 +1,31 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Transaction from "./pages/Transaction";
 import NotFound from "./pages/NotFound";
 import Header from "./components/Header";
+import { useQuery } from "@apollo/client";
+import { GET_AUTHENTICATED_USER } from "./graphql/queries/user.query";
 
 const App = () => {
   const authUser = false;
+  const { loading, data } = useQuery(GET_AUTHENTICATED_USER);
+  console.log("Data: ", data);
   return (
     <>
-      {authUser && <Header />}
+      {data?.authUser && <Header />}
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={data?.authUser ? <Home /> : <Navigate to="/login" />}
+        />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/transaction/:id" element={<Transaction />} />
+        <Route
+          path="/transaction/:id"
+          element={data?.authUser ? <Transaction /> : <Navigate to="/login" />}
+        />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
